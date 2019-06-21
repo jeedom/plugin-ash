0078f4239209d1d1a7d07d02fdd669363f40f7fc
Loïc <loic@jeedom.com>
2019-06-21 10:53:38 +0200

    support multi box object voice assit

--- core/php/jeeAsh.php
+++ core/php/jeeAsh.php
@@ -1,23 +1,27 @@
 <?php
 
 /* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
 require_once dirname(__FILE__) . "/../../../../core/php/core.inc.php";
 if (init('apikey') != '') {
-	if (!jeedom::apiAccess(init('apikey'), 'ash')) {
+	$apikey = init('apikey');
+	if(isset($apikey) && strpos($apikey],'-') !== false){
+		$apikey = substr($apikey, 0, strpos($apikey, '-'));
+	}
+	if (!jeedom::apiAccess($apikey, 'ash')) {
 		echo __('Vous n\'etes pas autorisé à effectuer cette action. Clef API invalide. Merci de corriger la clef API sur votre page profils du market et d\'attendre 24h avant de réessayer.', __FILE__);
 		die();
 	} else {
@@ -27,6 +31,9 @@ if (init('apikey') != '') {
 }
 header('Content-type: application/json');
 $data = json_decode(file_get_contents('php://input'), true);
+if(isset($data['apikey']) && strpos($data['apikey'],'-') !== false){
+	$data['apikey'] = substr($data['apikey'], 0, strpos($data['apikey'], '-'));
+}
 if (!isset($data['apikey']) || !jeedom::apiAccess($data['apikey'], 'ash')) {
 	echo json_encode(ash::buildErrorResponse($data, 'INTERNAL_ERROR'));
 	die();
