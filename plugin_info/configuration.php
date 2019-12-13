8493625f51923632effe788491d4087ec9c818fc
Loïc <loic@jeedom.com>
2019-12-13 13:57:58 +0100

    typo

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -1,19 +1,19 @@
 <?php
 /* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 include_file('core', 'authentification', 'php');
@@ -63,24 +63,24 @@ if (!isConnect()) {
 	</fieldset>
 </form>
 <script type="text/javascript">
-	$('#bt_sendConfigToMarket').on('click', function () {
-		$.ajax({
-			type: "POST",
-			url: "plugins/ash/core/ajax/ash.ajax.php",
-			data: {
-				action: "sendConfig",
-			},
-			dataType: 'json',
-			error: function (request, status, error) {
-				handleAjaxError(request, status, error);
-			},
-			success: function (data) {
-				if (data.state != 'ok') {
-					$('#div_alert').showAlert({message: data.result, level: 'danger'});
-					return;
-				}
-				$('#div_alert').showAlert({message: '{{Configuration envoyée avec succès. Merci d\'attendre 24h pour que la demande soit prise en compte}}', level: 'success'});
+$('#bt_sendConfigToMarket').on('click', function () {
+	$.ajax({
+		type: "POST",
+		url: "plugins/ash/core/ajax/ash.ajax.php",
+		data: {
+			action: "sendConfig",
+		},
+		dataType: 'json',
+		error: function (request, status, error) {
+			handleAjaxError(request, status, error);
+		},
+		success: function (data) {
+			if (data.state != 'ok') {
+				$('#div_alert').showAlert({message: data.result, level: 'danger'});
+				return;
 			}
-		});
+			$('#div_alert').showAlert({message: '{{Configuration envoyée avec succès.}}', level: 'success'});
+		}
 	});
+});
 </script>
