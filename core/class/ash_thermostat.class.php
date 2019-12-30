ad4dc7cbda5c0ba04b9e27cacdf6b5bb89077c82
Loïc <loic@jeedom.com>
2019-12-30 13:38:53 +0100

    Update ash_thermostat.class.php

--- core/class/ash_thermostat.class.php
+++ core/class/ash_thermostat.class.php
@@ -51,7 +51,7 @@ class ash_thermostat {
 					$return['capabilities']['Alexa.ThermostatController'] = array(
 						'type' => 'AlexaInterface',
 						'interface' => 'Alexa.ThermostatController',
-						'version' => 3,
+						'version' => "3",
 						'properties' => array(
 							'supported' => array(),
 							'proactivelyReported' => false,
@@ -71,7 +71,7 @@ class ash_thermostat {
 					$return['capabilities']['Alexa.ThermostatController'] = array(
 						'type' => 'AlexaInterface',
 						'interface' => 'Alexa.ThermostatController',
-						'version' => 3,
+						'version' => "3",
 						'properties' => array(
 							'supported' => array(),
 							'proactivelyReported' => false,
@@ -90,7 +90,7 @@ class ash_thermostat {
 				$return['capabilities']['Alexa.TemperatureSensor'] = array(
 					'type' => 'AlexaInterface',
 					'interface' => 'Alexa.TemperatureSensor',
-					'version' => 3,
+					'version' => "3",
 					'properties' => array(
 						'supported' => array(
 							array('name' => 'temperature'),
