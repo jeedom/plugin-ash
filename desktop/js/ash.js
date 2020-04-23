926dddbd9d8cbd42be826b2775e5ac38e1899b3f
Loïc <loic@jeedom.com>
2020-04-23 16:40:16 +0200

    ui improvements

--- desktop/js/ash.js
+++ desktop/js/ash.js
@@ -285,10 +285,12 @@ function addScene(_scene) {
   div += '<input class="sceneAttr form-control" data-l1key="options" data-l2key="group" placeholder="{{Groupe objet (option nécéssitant un compte market spécifique)}}" />';
   div += '</div>';
   div += '<div class="col-sm-6">';
-  div += '<div class="btn-group pull-right" role="group">';
-  div += '<a class="btn btn-sm bt_removeScene btn-primary"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>';
+  div += '<div class="input-group pull-right" style="display:inline-flex">';
+  div += '<span class="input-group-btn">';
+  div += '<a class="btn btn-sm bt_removeScene btn-primary roundedLeft"><i class="fa fa-minus-circle"></i> {{Supprimer}}</a>';
   div += '<a class="btn btn-sm bt_addInAction btn-success"><i class="fa fa-plus-circle"></i> {{Action d\'entrée}}</a>';
-  div += '<a class="btn btn-danger btn-sm bt_addOutAction"><i class="fa fa-plus-circle"></i> {{Action de sortie}}</a>';
+  div += '<a class="btn btn-danger btn-sm bt_addOutAction roundedRight"><i class="fa fa-plus-circle"></i> {{Action de sortie}}</a>';
+  div += '</span>';
   div += '</div>';
   div += '</div>';
   div += '</div>';
